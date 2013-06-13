19e57ae8cce68e8e9ec81cad097f6d7f06ad6f56
Isaac Grant <isaac@silverorange.com>
2013-06-13 20:16:08 +0000

    svn add breakdown on my part. Add the exception to teh exceptions folder that was created for it.
    svn commit r81720

---
+++ NewsFlash/exceptions/NewsFlashTwitterSourceException.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'Swat/exceptions/SwatException.php';
+
+/**
+ * Exception caused by twitter source in NewsFlash.
+ *
+ * @package   NewsFlash
+ * @copyright 2013 silverorange
+ */
+class NewsFlashTwitterSourceException extends SwatException
+{
+}
+
+?>
